d892e1c334d746f5d86bedd4206cd2f7957dfe2c
AKGarland <amberkgarland@gmail.com>
2018-07-11 15:00:59 +0100

    Sign up functionality inputs into database

--- src/main/java/DBConnector.java
+++ src/main/java/DBConnector.java
@@ -5,18 +5,21 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 
 public class DBConnector {
+
     public static boolean addUser(YeetUpUser user) {
+
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection(
                     "jdbc:mysql://db-workshop.public-dev.zuto.cloud/bootcamp3",
                     "bootcamp3", "TelephoneWeek");
 
             if (con != null) {
                 Statement stmt = con.createStatement();
-                stmt.execute("insert into Users(forename, surname, email, password, subscribe) values("+
-                        user.getForename() + ", " + user.getSurname() + ", "+ user.getEmail() + ", "+
-                        user.getPassword() + ", "+ user.isSubscribe() + ", " + ")");
+                String statement = "insert into Users(forename, surname, email, password, subscribe) values('"+
+                        user.getForename() + "', '" + user.getSurname() + "', '"+ user.getEmail() + "', '"+
+                        user.getPassword() + "', "+ user.getSubscribe() + ")";
+                System.out.println(statement);
+                stmt.execute(statement);
             }
 
             con.close();
